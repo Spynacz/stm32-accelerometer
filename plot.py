30c7ff2d9dc2e4a346338a90c324b2c44aef40a3
Spynacz <63506536+Spynacz@users.noreply.github.com>
2023-06-14 21:29:19 +0200

    Remove thread function

--- plot.py
+++ plot.py
@@ -12,41 +12,37 @@ ser = serial.Serial()
 axis = [[] for y in range(3)] 
 
 def plot():
-    fig.clear()
-    line = ser.readline().decode("UTF-8") 
-    nums = line.split(' ') 
-    axis[0].append(int(nums[0])) 
-    axis[1].append(int(nums[1])) 
-    axis[2].append(int(nums[2])) 
-        
-    ax = fig.add_subplot(111)
-    ax.plot(axis[0], color='blue', label="X") 
-    ax.plot(axis[1], color='green', label="Y") 
-    ax.plot(axis[2], color='red', label="Z") 
-
-    ax.legend(loc='lower left')
-    ax.set_ylabel("acceleration")
-
-    canvas.get_tk_widget().pack()
-    canvas.draw()
-    root.loadtk()
-
-
-def thread():
     global looping
-    while(looping):
-        plot()
+    while (looping):
+        fig.clear()
+        line = ser.readline().decode("UTF-8") 
+        nums = line.split(' ') 
+        axis[0].append(int(nums[0])) 
+        axis[1].append(int(nums[1])) 
+        axis[2].append(int(nums[2])) 
+            
+        ax = fig.add_subplot(111)
+        ax.plot(axis[0], color='blue', label="X") 
+        ax.plot(axis[1], color='green', label="Y") 
+        ax.plot(axis[2], color='red', label="Z") 
+
+        ax.legend(loc='lower left')
+        ax.set_ylabel("acceleration")
+
+        canvas.get_tk_widget().pack()
+        canvas.draw()
+        root.loadtk()
 
 
 def start(): 
-    if(not ser.is_open):
+    if (not ser.is_open):
         ser.baudrate = 115200 
         ser.port = combo.get()
         ser.open() 
         ser.write('1'.encode('UTF-8')) 
         global looping 
         looping = True 
-        th = threading.Thread(target=thread)
+        th = threading.Thread(target=plot)
         th.start()
 
  
