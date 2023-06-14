3c01866e366b57cf17fb93634060afd8d721866d
Spynacz <63506536+Spynacz@users.noreply.github.com>
2023-06-14 12:37:48 +0200

    Add port selection to plot script

--- plot.py
+++ plot.py
@@ -1,61 +1,78 @@
-import serial
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
-import matplotlib.patches as patches
+import threading
+import serial 
+import matplotlib.patches as patches 
+from matplotlib.figure import Figure
+from tkinter import * 
+from tkinter import ttk 
+import serial.tools.list_ports 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
+ 
+ser = serial.Serial() 
 
-mpl.use('tkagg')
+axis = [[] for y in range(3)] 
 
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = '/dev/pts/5'
-ser.open()
-# ser.readline()
-axis = [[] for y in range(3)]
+def plot():
+    fig.clear()
+    line = ser.readline().decode("UTF-8") 
+    nums = line.split(' ') 
+    axis[0].append(int(nums[0])) 
+    axis[1].append(int(nums[1])) 
+    axis[2].append(int(nums[2])) 
+        
+    ax = fig.add_subplot(111)
+    ax.plot(axis[0], color='blue', label="X") 
+    ax.plot(axis[1], color='green', label="Y") 
+    ax.plot(axis[2], color='red', label="Z") 
 
-fig, ax = plt.subplots(1, 1, num="STM32 Accelerometer")
-ax.set_title("Accelerometer readout")
+    ax.legend(loc='lower left')
+    ax.set_ylabel("acceleration")
 
-blue_patch = patches.Patch(color='blue', label='X')
-green_patch = patches.Patch(color='green', label='Y')
-red_patch = patches.Patch(color='red', label='Z')
-plt.legend(handles=[blue_patch, green_patch, red_patch], loc='center left', bbox_to_anchor=(1, 0.5))
-
-plt.subplots_adjust(bottom=0.2, right=0.8)
-
-looping = False
+    canvas.get_tk_widget().pack()
+    canvas.draw()
+    root.loadtk()
 
 
-def start(event):
-    ser.write('1'.encode('UTF-8'))
+def thread():
     global looping
-    looping = True
-    plt.ion()
-    while looping:
-        line = ser.readline().decode("UTF-8")
-        nums = line.split(' ')
-        axis[0].append(int(nums[0]))
-        axis[1].append(int(nums[1]))
-        axis[2].append(int(nums[2]))
-        ax.plot(axis[0], color='blue', label="X")
-        ax.plot(axis[1], color='green', label="Y")
-        ax.plot(axis[2], color='red', label="Z")
-        plt.draw()
-        plt.pause(0.1)
+    while(looping):
+        plot()
 
 
-def stop(event):
-    ser.write('0'.encode('UTF-8'))
-    global looping
-    looping = False
+def start(): 
+    if(not ser.is_open):
+        ser.baudrate = 115200 
+        ser.port = combo.get()
+        ser.open() 
+        ser.write('1'.encode('UTF-8')) 
+        global looping 
+        looping = True 
+        th = threading.Thread(target=thread)
+        th.start()
+
+ 
+def stop(): 
+    ser.write('0'.encode('UTF-8')) 
+    global looping 
+    looping = False 
+    ser.close() 
 
 
-axstart = plt.axes([0.4, 0.05, 0.1, 0.075])
-bstart = Button(axstart, "Start")
-bstart.on_clicked(start)
+looping = False 
+ 
+root = Tk()
+root.title('STM32 accelerator')
+fig = Figure(figsize=(5,5))
+canvas = FigureCanvasTkAgg(fig, master=root)
+butt1 = ttk.Button(root, text="Start", command=start) 
+butt1.pack(side=BOTTOM) 
+butt2 = ttk.Button(root, text="Stop", command=stop) 
+butt2.pack(side=BOTTOM)
+combo = ttk.Combobox(root)
+combo.pack(side=BOTTOM)
 
-axstop = plt.axes([0.5, 0.05, 0.1, 0.075])
-bstop = Button(axstop, "Stop")
-bstop.on_clicked(stop)
+ports=[] 
+for port in serial.tools.list_ports.comports(): 
+   ports.append(port.name) 
+combo['values'] = ports 
 
-plt.show()
+root.mainloop()
